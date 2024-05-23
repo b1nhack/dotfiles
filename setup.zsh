355aad67c068b238165cf5407b381a7503e8d075
b1n <120295547+b1nhack@users.noreply.github.com>
2024-05-23 16:26:05 +0800

    build: add setup script

---
+++ setup.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+ln -s ../zsh/plugins/zsh-fsh/themes/catppuccin-macchiato.ini ~/.config/fsh/catppuccin-macchiato.ini
+fast-theme XDG:catppuccin-macchiato
+
+ln -s ./bat/themes/Catppuccin\ Macchiato.tmTheme ~/.config/bat/themes/Catppuccin\ Macchiato.tmTheme
+bat cache --build
+
+ln -s ./gitui/theme/macchiato.ron ~/.config/gitui/theme.ron
+
+kitty +kitten themes --reload-in=all Catppuccin-Macchiato
