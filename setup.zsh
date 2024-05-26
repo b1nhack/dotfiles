c4bffe98c3cca5ba6e0361c20a4b211b36cc6c43
b1n <b1nhack@proton.me>
2024-05-26 23:18:44 +0800

    feat(setup): force link

--- setup.zsh
+++ setup.zsh
@@ -3,14 +3,12 @@
 if [[ ! -d "$HOME/.config/fsh" ]]; then
 	mkdir -p "~/.config/fsh"
 fi
-ln -s ../zsh/plugins/zsh-fsh/themes/catppuccin-macchiato.ini ~/.config/fsh/catppuccin-macchiato.ini
-fast-theme XDG:catppuccin-macchiato
+ln -sf ../zsh/plugins/zsh-fsh/themes/catppuccin-macchiato.ini ~/.config/fsh/catppuccin-macchiato.ini
+# fast-theme XDG:catppuccin-macchiato
 
-ln -s ./bat/themes/Catppuccin\ Macchiato.tmTheme ~/.config/bat/themes/Catppuccin\ Macchiato.tmTheme
-bat cache --build
+ln -sf ./bat/themes/Catppuccin\ Macchiato.tmTheme ~/.config/bat/themes/Catppuccin\ Macchiato.tmTheme
+# bat cache --build
 
-ln -s ./gitui/theme/macchiato.ron ~/.config/gitui/theme.ron
+ln -sf ./gitui/theme/macchiato.ron ~/.config/gitui/theme.ron
 
-kitty +kitten themes --reload-in=all Catppuccin-Macchiato
-
-ln -s ./.config/zsh/.zshenv ~/.zshenv
+ln -sf ./.config/zsh/.zshenv ~/.zshenv
