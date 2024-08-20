2462668185b768ee794b8570d1cc00375330a66a
b1n <b1n@b1n.io>
2024-08-20 13:55:40 +0800

    refactor(setup): use absolute paths

--- setup.zsh
+++ setup.zsh
@@ -3,19 +3,19 @@
 if [[ ! -d "$HOME/.config/fsh" ]]; then
 	mkdir -p "~/.config/fsh"
 fi
-ln -sf ../zsh/plugins/zsh-fsh/themes/catppuccin-macchiato.ini ~/.config/fsh/catppuccin-macchiato.ini
+ln -sf ~/.config/zsh/plugins/zsh-fsh/themes/catppuccin-macchiato.ini ~/.config/fsh/catppuccin-macchiato.ini
 # fast-theme XDG:catppuccin-macchiato
 
-ln -sf ./bat/themes/Catppuccin\ Macchiato.tmTheme ~/.config/bat/themes/Catppuccin\ Macchiato.tmTheme
+ln -sf ~/.config/bat/themes/bat/themes/Catppuccin\ Macchiato.tmTheme ~/.config/bat/themes/Catppuccin\ Macchiato.tmTheme
 # bat cache --build
 
-ln -sf ./gitui/theme/macchiato.ron ~/.config/gitui/theme.ron
+ln -sf ~/.config/gitui/gitui/themes/catppuccin-macchiato.ron ~/.config/gitui/theme.ron
 
-ln -sf ./.config/zsh/.zshenv ~/.zshenv
+ln -sf ~/.config/zsh/.zshenv ~/.zshenv
 
-ln -sf ../.config/gnupg/gpg.conf ~/.gnupg/gpg.conf
-ln -sf ../.config/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
-ln -sf ../.config/gnupg/dirmngr.conf ~/.gnupg/dirmngr.conf
+ln -sf ~/.config/gnupg/gpg.conf ~/.gnupg/gpg.conf
+ln -sf ~/.config/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+ln -sf ~/.config/gnupg/dirmngr.conf ~/.gnupg/dirmngr.conf
 
 if [[ ! -d "$HOME/.config/aerc/stylesets" ]]; then
 	mkdir -p "~/.config/aerc/stylesets"
