cb6c9a2632f446db7f5b395d7a5f397e3348223f
Xingquan Liu <b1n@b1n.io>
2025-04-24 14:15:57 -0400

    feat: cleanup

--- aerc/signature.sig
+++
@@ -1,5 +0,0 @@
-
-
--- 
-Xingquan Liu
-
--- setup.zsh
+++ setup.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 if [[ ! -d "$HOME/.config/fsh" ]]; then
-	mkdir -p "~/.config/fsh"
+	mkdir ~/.config/fsh
 fi
 ln -sf ~/.config/zsh/plugins/zsh-fsh/themes/catppuccin-macchiato.ini ~/.config/fsh/catppuccin-macchiato.ini
 # fast-theme XDG:catppuccin-macchiato
@@ -9,17 +9,10 @@ ln -sf ~/.config/zsh/plugins/zsh-fsh/themes/catppuccin-macchiato.ini ~/.config/f
 ln -sf ~/.config/bat/themes/bat/themes/Catppuccin\ Macchiato.tmTheme ~/.config/bat/themes/Catppuccin\ Macchiato.tmTheme
 # bat cache --build
 
-ln -sf ~/.config/gitui/gitui/themes/catppuccin-macchiato.ron ~/.config/gitui/theme.ron
-
 ln -sf ~/.config/zsh/.zshenv ~/.zshenv
 
 ln -sf ~/.config/gnupg/gpg.conf ~/.gnupg/gpg.conf
 ln -sf ~/.config/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 ln -sf ~/.config/gnupg/dirmngr.conf ~/.gnupg/dirmngr.conf
-
-if [[ ! -d "$HOME/.config/aerc/stylesets" ]]; then
-	mkdir -p "~/.config/aerc/stylesets"
-fi
-ln -sf ~/.config/aerc/aerc/dist/catppuccin-macchiato ~/.config/aerc/stylesets/catppuccin-macchiato
 
 ln -sf ~/.config/eza/eza-themes/themes/catppuccin.yml ~/.config/eza/theme.yml
